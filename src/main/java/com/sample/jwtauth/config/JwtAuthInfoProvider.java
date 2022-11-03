a89a3652b65b548fbec3969daeb4d45c58e2242c
budarov <budarov@haulmont.com>
2022-11-03 16:27:41 +0400

    use constant for preferred_username

--- src/main/java/com/sample/jwtauth/config/JwtAuthInfoProvider.java
+++ src/main/java/com/sample/jwtauth/config/JwtAuthInfoProvider.java
@@ -1,8 +1,8 @@
 package com.sample.jwtauth.config;
 
 import com.amplicode.core.auth.AuthenticationInfoProvider;
-import com.sample.jwtauth.controller.UserInfoController;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.oidc.StandardClaimNames;
 import org.springframework.security.oauth2.jwt.Jwt;
 
 class JwtAuthInfoProvider implements AuthenticationInfoProvider {
@@ -11,7 +11,7 @@ class JwtAuthInfoProvider implements AuthenticationInfoProvider {
     public String getAuthenticatedUsername() {
         Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         if (principal instanceof Jwt) {
-            final String username = ((Jwt) principal).getClaimAsString("preferred_username");
+            final String username = ((Jwt) principal).getClaimAsString(StandardClaimNames.PREFERRED_USERNAME);
             return username;
         }
         return null;
