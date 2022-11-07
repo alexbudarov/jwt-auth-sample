aa327b96e6d97a029aea288161c6547d9020e768
budarov <budarov@haulmont.com>
2022-11-07 12:23:35 +0400

    scenario: backend: create SecurityConfiguration by SpringBuddy wizard

---
+++ src/main/java/com/sample/jwtauth/config/WebSecurityConfiguration.java
@@ -0,0 +1,35 @@
+package com.sample.jwtauth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        //OAuth 2.0 Login
+        http.oauth2Login(Customizer.withDefaults());
+        //Authorize Requests
+        http.authorizeRequests(authorizeRequests -> authorizeRequests
+                .antMatchers("/graphql").permitAll()
+                .antMatchers("/graphql/**").permitAll());
+        //Headers management
+        http.headers(Customizer.withDefaults());
+        //JWT Token
+        http.oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer
+                .jwt(Customizer.withDefaults())
+        );
+        //Anonymous
+        http.anonymous(Customizer.withDefaults());
+        //CSRF
+        http.csrf(csrf -> csrf
+                .disable());
+        return http.build();
+    }
+}
