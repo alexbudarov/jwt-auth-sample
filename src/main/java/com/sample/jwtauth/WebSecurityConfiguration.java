67c9e82ad9efeb7971df4307f593310a2f465c57
budarov <budarov@haulmont.com>
2022-11-15 12:53:20 +0400

    cognito scenario: additional backend changes

--- src/main/java/com/sample/jwtauth/WebSecurityConfiguration.java
+++ src/main/java/com/sample/jwtauth/WebSecurityConfiguration.java
@@ -1,16 +1,46 @@
 package com.sample.jwtauth;
 
+import com.amplicode.core.auth.AuthenticationInfoProvider;
+import com.amplicode.core.auth.JwtAuthenticationInfoProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.core.oidc.StandardClaimNames;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Arrays;
 
 @EnableWebSecurity
 @Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class WebSecurityConfiguration {
 
+    @Bean
+    public AuthenticationInfoProvider authenticationInfoProvider() {
+        JwtAuthenticationInfoProvider jwtAuthenticationInfoProvider = new JwtAuthenticationInfoProvider();
+        jwtAuthenticationInfoProvider.setClaimNames(Arrays.asList(
+                StandardClaimNames.PREFERRED_USERNAME,
+                "cognito:username"
+        ));
+        return jwtAuthenticationInfoProvider;
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        grantedAuthoritiesConverter.setAuthoritiesClaimName("cognito:groups");
+        grantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
+
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
+
+        return jwtAuthenticationConverter;
+    }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         //Authorize Requests
